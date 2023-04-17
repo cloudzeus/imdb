9a3b08f6059d2d04618c7b1588aee90facbbc95c
cloudzeus <gkozyris@cloudzeus.eu>
2023-04-17 14:58:10 +0300

    tailwind installation

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,11 +2,8 @@ import './globals.css'
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang="el">
+
       <head />
       <body>{children}</body>
     </html>
